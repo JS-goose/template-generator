8b0ac7fba09778646ff28e0f9f6fec9539ff599a
Jonathan Sexton <joncsexton@gmail.com>
2024-10-22 09:54:05 -0500

    added ignore timers module during webpack build

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        timers: false, // ignore timers module
+      }
+    }
+  }
 })
