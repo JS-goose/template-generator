1ef4cfb942c9b854b0b2d900d2a31c61d13f2196
Jonathan Sexton <joncsexton@gmail.com>
2024-10-23 15:25:09 -0500

    webpack config updates

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
     resolve: {
       fallback: {
-        timers: false, // ignore timers module
-        stream: false, // ignore stream module 
-      }
-    }
-  }
-})
+        stream: require.resolve('stream-browserify'), // For sax.js
+        process: require.resolve('process/browser'), // For process
+        buffer: require.resolve('buffer/'), // For Buffer (if needed)
+        timers: require.resolve('timers-browserify'), // For timers
+        // Add any other polyfills you might need
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+        // If you need to provide any other globals, add them here
+      }),
+    ],
+  },
+});
