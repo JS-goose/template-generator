1bffd6ea14849ebfe5dcbffdfd8337d878f037be
Jonathan Sexton <joncsexton@gmail.com>
2024-10-26 07:51:36 -0500

    new component

---
+++ src/components/FeedSelector.vue
@@ -0,0 +1,24 @@
+<template>
+  <section></section>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        data: [],
+      };
+    },
+    methods: {},
+    computed: {
+      computedExample() {},
+    },
+    watch: {
+      message(newValue, oldValue) {
+        console.log(newValue, oldValue);
+      },
+    },
+  };
+</script>
+<style lang='scss' scoped>
+  @import "";
+</style>
