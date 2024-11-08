d0f0b97ba980a27b697f35b98569c57e8b80ab85
Jonathan Sexton <joncsexton@gmail.com>
2024-11-08 09:32:40 -0600

    added brand assets

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,17 @@ export default {
 /* TODO Apply Cloudinary styling guide found here: https://cloudinary.com/brand  */
 #app {
   font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  /* * Primary CLD Colors */
+  --cldBlue: #3448C5;
+  --cldAegeanBlue: #101926;
+  --cldCetaceanBlue: #0C163B;
+  --cldMidnightBlue: #3F5FFF;
+  --cldSkyBlue: #0D9AFF;
+  --cldTurquoiseBlue: #48C4D8;
+  /* * Secondary CLD Colors */
+  --cldCoral: #FF5050;
+  --cldYellow: #F7BC00;
+  --cldSlate: #162436;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
