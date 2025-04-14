25d211c9ab3b6c384e30205bacc5e5a912e151cd
Jonathan <joncsexton@gmail.com>
2025-04-14 16:47:26 -0500

    getting started component creation

---
+++ src/components/GettingStarted.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Getting Started</h1>
+    <ol>
+      <li>
+        Pull individual feeds using the action buttons on the top right or all
+        feeds using the "Pull All RSS Feeds" button on the left
+      </li>
+      <li>
+        Once feeds are pulled, you can clear individual feeds with the "Clear"
+        button beside each product or use the
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style>
+</style>
